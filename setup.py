b86d65c2160ae183d22db5df631183195ad6caa7
Cujoko <cujoko@gmail.com>
2016-01-02 04:47:55 +0900

    Улучшил setup.py.

--- setup.py
+++ setup.py
@@ -6,11 +6,30 @@ from setuptools import setup
 
 setup(
     name='decompiler1cwrapper',
-    url='https://github.com/Cujoko/decompiler1cwrapper',
+
     version=decompiler1cwrapper.__version__,
+
+    description='Decompile and compile utilities for 1C:Enterprise',
+
+    url='https://github.com/Cujoko/decompiler1cwrapper',
 
     author='Cujoko',
     author_email='cujoko@gmail.com',
+
+    license='MIT',
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+
+        'Intended Audience :: Developers',
+        'Topic :: Utilities',
+
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 3.5'
+    ],
+
+    keywords='1c v8reader v8unpack gcomp',
 
     py_modules=['decompiler1cwrapper'],
 
@@ -21,3 +40,4 @@ setup(
         ]
     }
 )
+

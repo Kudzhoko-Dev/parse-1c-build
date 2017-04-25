18a0531c4f8b70f4852f02b22be8c66dcd380c81
Cujoko <cujoko@gmail.com>
2017-04-26 04:09:42 +0900

    Рефакторинг.

--- parse_1c_build.py
+++ parse_1c_build.py
@@ -10,7 +10,7 @@ import shutil
 import subprocess
 
 
-__version__ = '2.1.0'
+__version__ = '2.1.1'
 
 APP_AUTHOR = 'util-1c'
 APP_NAME = 'parse-1c-build'
@@ -38,28 +38,28 @@ class Processor:
         self.general_section_name = 'General'
         self.general_section = self.config[self.general_section_name]
 
-        self.exe_1c = None
+        self.exe_1c_file_path = None
         if '1C' in self.general_section:
-            self.exe_1c = Path(self.general_section['1C'])
-        if self.exe_1c is None or not self.exe_1c.is_file():
-            self.exe_1c = Processor.get_last_exe_1c()
-            if self.exe_1c is None:
+            self.exe_1c_file_path = Path(self.general_section['1C'])
+        if self.exe_1c_file_path is None or not self.exe_1c_file_path.is_file():
+            self.exe_1c_file_path = Processor.get_last_exe_1c()
+            if self.exe_1c_file_path is None:
                 raise SettingsError('1C:Enterprise 8 does not exist!')
 
-        self.ib = Path(self.general_section['IB'])
-        if not self.ib.is_dir():
+        self.ib_dir_path = Path(self.general_section['IB'])
+        if not self.ib_dir_path.is_dir():
             raise SettingsError('Service information base does not exist!')
 
-        self.v8_reader = Path(self.general_section['V8Reader'])
-        if not self.v8_reader.is_file():
+        self.v8_reader_file_path = Path(self.general_section['V8Reader'])
+        if not self.v8_reader_file_path.is_file():
             raise SettingsError('V8Reader does not exist!')
 
-        self.v8_unpack = Path(self.general_section['V8Unpack'])
-        if not self.v8_unpack.is_file():
+        self.v8_unpack_file_path = Path(self.general_section['V8Unpack'])
+        if not self.v8_unpack_file_path.is_file():
             raise SettingsError('V8Unpack does not exist!')
 
-        self.gcomp = Path(self.general_section['GComp'])
-        if not self.gcomp.is_file():
+        self.gcomp_file_path = Path(self.general_section['GComp'])
+        if not self.gcomp_file_path.is_file():
             raise SettingsError('GComp does not exist!')
 
     @staticmethod
@@ -80,10 +80,10 @@ class Processor:
     def get_last_exe_1c():
         result = None
 
-        estart_path = Path(user_data_dir('1CEStart', '1C', roaming=True)) / '1CEStart.cfg'
+        estart_file_path = Path(user_data_dir('1CEStart', '1C', roaming=True)) / '1CEStart.cfg'
         installed_location_paths = []
-        if estart_path.is_file():
-            with estart_path.open(encoding='utf-16') as estart_file:
+        if estart_file_path.is_file():
+            with estart_file_path.open(encoding='utf-16') as estart_file:
                 for line in estart_file.readlines():
                     key_and_value = line.split('=')
                     if key_and_value[0] == 'InstalledLocation':
@@ -100,9 +100,9 @@ class Processor:
                             if p2.is_file():
                                 platform_versions.append((version_as_number, p2))
 
-            platform_versions1 = sorted(platform_versions, key=lambda x: x[0], reverse=True)
-            if platform_versions1:
-                result = platform_versions1[0][1]
+            platform_versions_reversed = sorted(platform_versions, key=lambda x: x[0], reverse=True)
+            if platform_versions_reversed:
+                result = platform_versions_reversed[0][1]
         else:
             raise SettingsError('1CEStart.cfg file does not exist!')
 
@@ -119,45 +119,45 @@ class Parser(Processor):
         self.argparser.add_argument('input', nargs='?')
         self.argparser.add_argument('output', nargs='?')
 
-    def parse(self, input_path: Path, output_path: Path):
+    def parse(self, input_file_path: Path, output_dir_path: Path):
         with tempfile.NamedTemporaryFile('w', encoding='cp866', suffix='.bat', delete=False) as bat_file:
             bat_file.write('@echo off\n')
-            in_path_suffix_lower = input_path.suffix.lower()
-            if in_path_suffix_lower in ['.epf', '.erf']:
+            input_file_path_suffix_lower = input_file_path.suffix.lower()
+            if input_file_path_suffix_lower in ['.epf', '.erf']:
                 bat_file.write('"{}" /F"{}" /DisableStartupMessages /Execute"{}" {}'.format(
-                    str(self.exe_1c),
-                    str(self.ib),
-                    str(self.v8_reader),
+                    str(self.exe_1c_file_path),
+                    str(self.ib_dir_path),
+                    str(self.v8_reader_file_path),
                     '/C"decompile;pathtocf;{};pathout;{};shutdown;convert-mxl2txt;"'.format(
-                        str(input_path),
-                        str(output_path)
+                        str(input_file_path),
+                        str(output_dir_path)
                     )
                 ))
-            elif in_path_suffix_lower in ['.ert', '.md']:
-                input_path_ = input_path
+            elif input_file_path_suffix_lower in ['.ert', '.md']:
+                input_file_path_ = input_file_path
 
-                if in_path_suffix_lower == '.md':
-                    tmp_dir = tempfile.mkdtemp()
-                    input_path_ = Path(shutil.copy(str(input_path_), tmp_dir))
+                if input_file_path_suffix_lower == '.md':
+                    temp_dir_name = tempfile.mkdtemp()
+                    input_file_path_ = Path(shutil.copy(str(input_file_path_), temp_dir_name))
 
                 bat_file.write('"{}" -d -F "{}" -DD "{}"'.format(
-                    str(self.gcomp),
-                    str(input_path_),
-                    str(output_path)
+                    str(self.gcomp_file_path),
+                    str(input_file_path_),
+                    str(output_dir_path)
                 ))
 
         exit_code = subprocess.check_call(['cmd.exe', '/C', str(bat_file.name)])
         if not exit_code == 0:
-            raise Exception('Parsing "{}" is failed!'.format(str(input_path)))
+            raise Exception('Parsing "{}" is failed!'.format(str(input_file_path)))
 
         Path(bat_file.name).unlink()
 
     def run(self):
         args = self.argparser.parse_args()
 
-        input_path = Path(args.input)
-        output_path = Path(args.output)
-        self.parse(input_path, output_path)
+        input_file_path = Path(args.input)
+        output_dir_path = Path(args.output)
+        self.parse(input_file_path, output_dir_path)
 
 
 class Builder(Processor):
@@ -167,56 +167,56 @@ class Builder(Processor):
         self.argparser.add_argument('input', nargs='?')
         self.argparser.add_argument('output', nargs='?')
 
-    def get_temp_source_folder(self, input_path: Path):
-        temp_source_folder = Path(tempfile.mkdtemp())
-        if not temp_source_folder.is_dir():
-            temp_source_folder.mkdir(parents=True)
+    def get_temp_source_dir_path(self, input_dir_path: Path):
+        temp_source_dir_path = Path(tempfile.mkdtemp())
+        if not temp_source_dir_path.is_dir():
+            temp_source_dir_path.mkdir(parents=True)
         else:
-            shutil.rmtree(str(temp_source_folder), ignore_errors=True)
+            shutil.rmtree(str(temp_source_dir_path), ignore_errors=True)
 
-        renames_file = input_path / 'renames.txt'
+        renames_file_path = input_dir_path / 'renames.txt'
 
-        with renames_file.open(encoding='utf-8-sig') as file:
+        with renames_file_path.open(encoding='utf-8-sig') as file:
             for line in file:
                 names = line.split('-->')
 
-                new_path = temp_source_folder / names[0].strip()
-                new_folder_path = new_path.parent
+                new_path = temp_source_dir_path / names[0].strip()
+                new_dir_path = new_path.parent
 
-                if not new_folder_path.is_dir():
-                    new_folder_path.mkdir(parents=True)
+                if not new_dir_path.is_dir():
+                    new_dir_path.mkdir(parents=True)
 
-                old_path = input_path / names[1].strip()
+                old_path = input_dir_path / names[1].strip()
 
                 if old_path.is_dir():
-                    new_path = temp_source_folder / names[0].strip()
+                    new_path = temp_source_dir_path / names[0].strip()
                     shutil.copytree(str(old_path), str(new_path))
                 else:
                     shutil.copy(str(old_path), str(new_path))
 
-        return temp_source_folder
+        return temp_source_dir_path
 
-    def build_raw(self, temp_source_folder: Path, output_path: Path):
+    def build_raw(self, temp_source_dir_path: Path, output_file_path: Path):
         exit_code = subprocess.check_call([
-            str(self.v8_unpack),
+            str(self.v8_unpack_file_path),
             '-B',
-            str(temp_source_folder),
-            str(output_path)
+            str(temp_source_dir_path),
+            str(output_file_path)
         ])
 
         if not exit_code == 0:
-            raise Exception('Building "{}" is failed!'.format(str(output_path)))
+            raise Exception('Building "{}" is failed!'.format(str(output_file_path)))
 
-    def build(self, input_path: Path, output_path: Path):
-        temp_source_folder = self.get_temp_source_folder(input_path)
-        self.build_raw(temp_source_folder, output_path)
+    def build(self, input_dir_path: Path, output_file_path: Path):
+        temp_source_dir_path = self.get_temp_source_dir_path(input_dir_path)
+        self.build_raw(temp_source_dir_path, output_file_path)
 
     def run(self):
         args = self.argparser.parse_args()
 
-        input_path = Path(args.input)
-        output_path = Path(args.output)
-        self.build(input_path, output_path)
+        input_dir_path = Path(args.input)
+        output_file_path = Path(args.output)
+        self.build(input_dir_path, output_file_path)
 
 
 class Error(Exception):

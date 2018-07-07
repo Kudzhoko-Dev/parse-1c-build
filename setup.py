443cecc919c892c482d7f5834c74fea07916529d
Cujoko <cujoko@gmail.com>
2018-07-07 18:28:27 +0900

    Добавил в setup.py поддержку Python 3.7.

--- setup.py
+++ setup.py
@@ -21,6 +21,7 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Software Development',
         'Topic :: Utilities'
     ],

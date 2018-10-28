1e85ba10e030ff32ed6d67497102c5c23b0bbf1e
Cujoko <cujoko@gmail.com>
2018-10-29 06:30:19 +0900

    Исправит ошибки импорта. Изменит setup.py

--- parse_1c_build/__init__.py
+++ parse_1c_build/__init__.py
@@ -2,11 +2,11 @@
 from __future__ import absolute_import, unicode_literals
 
 # noinspection PyUnresolvedReferences
-from build import Builder
+from parse_1c_build.build import Builder
 # noinspection PyUnresolvedReferences
-from parse import Parser
+from parse_1c_build.parse import Parser
 
-__version__ = '4.4.0'
+__version__ = '4.4.1'
 
 APP_AUTHOR = 'util-1c'
 APP_NAME = 'parse-1c-build'
